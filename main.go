21785fbab4d4f7515622865f1fc18d123d02b47f
Y2Danil <mc_danil@inbox.ru>
2021-01-03 16:09:38 +1200

    ...

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 	_ "github.com/lib/pq"
@@ -488,12 +489,12 @@ func handleRequest() {
 	http.Handle("/", rtr)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	// heroku con
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "9000"
-	// }
-	// http.ListenAndServe(":" + port, context.ClearHandler(http.DefaultServeMux))
-	http.ListenAndServe(":8000", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+	}
+	http.ListenAndServe(":" + port, context.ClearHandler(http.DefaultServeMux))
+	//http.ListenAndServe(":8000", nil)
 }
 
 func main() {
