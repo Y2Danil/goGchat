706c9693f46727c551963546e4b9735ca6aea9cd
Y2Danil <mc_danil@inbox.ru>
2021-01-10 20:35:55 +1200

    make it better

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
 	"time"
 
-	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 	_ "github.com/lib/pq"
@@ -43,6 +43,16 @@ type User struct {
 	Color    string
 }
 
+type OPuser struct {
+	OP int16
+}
+
+type RegisterStruct struct {
+	Nick string
+	Pass1 string
+	Pass2 string
+}
+
 // LoginUser - авторизация
 type LoginUser struct {
 	Name     string
@@ -60,17 +70,24 @@ type Type struct {
 
 // Theme класс темы 
 type Theme struct {
-	ID        int
+	ID        int64
 	Title     string
 	MiniDop   string
 	Date      time.Time
-	MinOp     int
+	MinOp     int16
 	OnlyRead  bool
-	CreUserID int
+	CreUserID int64
 	Fixing    bool
-	TypeID    int
+	TypeID    int16
 	Anon      bool 
 }
+
+type ThemeTitleAndMinDopAndID struct {
+	Title   string
+	MiniDop string
+	ID      int64
+}
+
 
 // Message класс сообщений 
 type Message struct {
@@ -169,7 +186,7 @@ func PKCS5Padding(src []byte, blockSize int) []byte {
 func PKCS5UnPadding(src []byte) []byte {
 	var result []byte
 	var diap int
-
+	
 	length := len(src)
 	unpadding := int(src[length-1])
 	
@@ -180,7 +197,11 @@ func PKCS5UnPadding(src []byte) []byte {
 	}
 
 	if diap >= length {
-		diap = length-1
+		if length != 0 {
+			diap = length-1
+		} else {
+			diap = 1
+		}
 	}
 
 	result = src[:diap]
@@ -306,34 +327,113 @@ func getMessages(sqlCode string) []Message {
 	return Messages
 }
 
+func OpenType(w http.ResponseWriter, r *http.Request) {
+	type StructOpenType struct {
+		Title   string
+		Themes []ThemeTitleAndMinDopAndID
+	}
+
+	var userOP interface{}
+	var data = StructOpenType{}
+
+	session, errSession := store.Get(r, "User")
+
+	if session != nil && errSession == nil {
+		userOP = session.Values["OP"]
+	} else {
+		userOP = 0
+	}
+
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+
+	typeID := vars["id"]
+
+	db := conn()
+
+	Type, errThemeSQL := db.Query("SELECT min_op, title FROM \"Type\" WHERE id=$1", typeID)
+
+	if errThemeSQL != nil {
+		fmt.Fprintf(w, "Error404")
+	}
+
+	type ThemeMinOPandTitle struct {
+		MinOp int64
+		Title string
+	}
+
+	themeMinOp := ThemeMinOPandTitle{} 
+
+	for Type.Next() {
+		Type.Scan(&themeMinOp.MinOp, &themeMinOp.Title)
+	}
+
+	data.Title = themeMinOp.Title
+
+	STRuserOP := fmt.Sprintf("%s", userOP)
+	int64userOp, _ := strconv.Atoi(STRuserOP)
+
+	
+
+	if themeMinOp.MinOp <= int64(int64userOp) {
+		typeThemes, errThemeSQL := db.Query("SELECT title, mini_dop, id FROM \"Theme\" WHERE type_id=$1", typeID)
+
+		if errThemeSQL != nil {
+			fmt.Fprintf(w, "Error404")
+		}
+
+		var themeInType = []ThemeTitleAndMinDopAndID{}
+
+		for typeThemes.Next() {
+			t := ThemeTitleAndMinDopAndID{}
+			typeThemes.Scan(&t.Title, &t.MiniDop, &t.ID)
+
+			themeInType = append(themeInType, t)
+		}
+
+		data.Themes = themeInType
+
+		tmpl, err := template.ParseFiles("templates/type.html", "templates/header.html", "templates/footer.html")
+
+		if err != nil {
+			fmt.Fprintf(w, "Error404")
+		}
+
+		defer db.Close()
+
+		tmpl.ExecuteTemplate(w, "type", data)
+	} else {
+		defer db.Close()
+
+		defer http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+type TypeTitleAndID struct {
+	Title string
+	ID 		int
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	Types := getTypes("SELECT * FROM \"Type\"")
 
 	//var ThemeByType map[string]Theme
 	// TAT - ThemesAndTypes
-	var TAT map[string][]Theme
-	TAT = make(map[string][]Theme)
+	var TAT map[TypeTitleAndID][]Theme
+	TAT = make(map[TypeTitleAndID][]Theme)
 
 	for i := range Types {
-		Themes := getThemes((fmt.Sprintf("SELECT * FROM \"Theme\" WHERE type_id=%d", Types[i].ID)))
+		Themes := getThemes((fmt.Sprintf("SELECT * FROM \"Theme\" WHERE type_id=%d AND fixing=TRUE", Types[i].ID)))
 
-		var j string = Types[i].Title
+		j := TypeTitleAndID{
+			Title: Types[i].Title,
+			ID: Types[i].ID, 
+		}
 		TAT[j] = Themes
 	}
 	fmt.Println(TAT)
 
 	tmpl, err := template.ParseFiles("templates/homePage.html", "templates/header.html", "templates/footer.html")
-	// for i, value := range TAT {
-	// 	fmt.Print(i)
-	// 	fmt.Print(":\n")
-	// 	for j := range value {
-	// 		fmt.Print("   ")
-	// 		fmt.Print(value[j])
-	// 		fmt.Print("\n")
-	// 	}
-	// 	fmt.Print("\n")
-	// }
 
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
@@ -349,6 +449,12 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "index", TAT)
 }
 
+type DataTaDM struct {
+	ThemeInfo Theme
+	Messages  []DeshifrMessage
+	User      interface{}
+}
+
 func openTheme(w http.ResponseWriter, r *http.Request) {
 	Vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
@@ -358,109 +464,180 @@ func openTheme(w http.ResponseWriter, r *http.Request) {
 	//WHERE rubric_id=%s;
 	Messages := getMessages(fmt.Sprintf("SELECT * FROM \"Message\" WHERE rubric_id=%s", ThemeID))
 	DeshifrMessages := []DeshifrMessage{}
+	userSession, errUserSession := store.Get(r, "User")
 
-	fmt.Println(Messages)
-	if Messages != nil {
-		var deKey interface{}
-		var stringDeKey string
-		var sessionKey *sessions.Session
+	if errUserSession != nil {
+		fmt.Fprintf(w, "Error404")
+	}
 
-		for i := range Messages {
-			fmt.Print(">>>")
-			fmt.Println(Messages[i].TextMsg)
-			sessionKey, _ = store.Get(r, "drYdvaj29dS2kEy$2wcdsgdsauw")
-			deKey = sessionKey.Values["Key"]
-			stringDeKey = fmt.Sprintf("%s", deKey)
-			fmt.Print(stringDeKey)
-			if stringDeKey == "" || len(stringDeKey) != 8 {
-				stringDeKey = "keyIdiot"
-			}
+	var user *sql.Rows
+	op := OPuser{}
+	var dbErr error
+	if userSession != nil {
+		db := conn()
+		userID := userSession.Values["ID"]
 
-			byteDeKey := []byte(stringDeKey)
-			fmt.Print("\n\n\n")
-			fmt.Println(byteDeKey)
-			// ivKey := []byte(string(byteDeKey) + strings.Repeat("0", 16))
-			// byteDeKey = []byte(string(byteDeKey) + strings.Repeat("0", 16))
-			ivKey := byteDeKey
-			fmt.Println(string(ivKey))
-			fmt.Println(ivKey)
-			deMsgText, errDes := DesDecryption(byteDeKey, ivKey, Messages[i].TextMsg)
+		user, dbErr = db.Query("SELECT OP FROM \"User\" WHERE id=$1", userID)
 
-			if errDes != nil {
+		if dbErr != nil {
+			fmt.Fprintf(w, "Error404")
+		}
+
+		for user.Next() {
+			err := user.Scan(&op.OP)
+
+			if err != nil {
 				fmt.Fprintf(w, "Error404")
 			}
-			//deMsgText := byteTextMessages
-			fmt.Print("--")
-
-			// if errDe != nil {
-			// 	fmt.Print("Err: ")
-			// 	log.Fatal(errDe)
-			// }
-
-			fmt.Print(">")
-			TextMsg := string(deMsgText)
-			fmt.Print("\n\n\n>")
-			fmt.Println(TextMsg)
-			UserInfo := getUsers(fmt.Sprintf("SELECT id, name, password, admin, op, moder, ava, status, color FROM \"User\" WHERE id=%d", Messages[i].UserID))
-
-			Msg := DeshifrMessage{Messages[i].ID, TextMsg, Messages[i].UserID, Messages[i].ThemeID, Messages[i].PubDate, UserInfo}
-			DeshifrMessages = append(DeshifrMessages, Msg)
-		} 
-	}
-
-	// DataTaDM Словарь для хранения Theme и DeshifrMsg, TaMD - Theme and DeshifrMsg
-
-	session, _ := store.Get(r, "User")
-	UserName := session.Values["Name"]
-	fmt.Print("UserName: ")
-	fmt.Println(UserName)
-	var data interface{}
-	if UserName == nil {
-		type DataTaDM struct {
-			ThemeInfo Theme
-			Messages  []DeshifrMessage
-			User      interface{}
 		}
 
-		data = DataTaDM{ThemeInfo: ThemeInfo, Messages: DeshifrMessages, User: nil}
-		fmt.Println("a")
+		fmt.Println(op)
+
+		defer db.Close()
 	} else {
-		type UserCookie struct {
-			Name interface{}
-			OP   interface{}
-			ID   interface{}
-		}
-		type DataTaDM struct {
-			ThemeInfo Theme
-			Messages  []DeshifrMessage
-			User 			UserCookie
-		}
-
-		cookieUserName := session.Values["Name"]
-		Name := cookieUserName
-		OP := session.Values["OP"]
-
-		data = DataTaDM{
-			ThemeInfo: ThemeInfo,
-			Messages: DeshifrMessages,
-			User: UserCookie{
-				Name:  Name,
-				OP: 	 OP,
-				ID:    session.Values["ID"],
-			},
-		}
-		fmt.Println("b")
+		op = OPuser{OP: 0}
 	}
-	fmt.Println(data)
-	
 
-	tmpl, err := template.ParseFiles("templates/theme.html", "templates/header.html", "templates/footer.html")
+	if op.OP >= ThemeInfo.MinOp {
+		if Messages != nil {
+			var deKey interface{}
+			var stringDeKey string
+			var sessionKey *sessions.Session
+
+			if ThemeInfo.Anon == true {
+
+			for i := range Messages {
+				sessionKey, _ = store.Get(r, "drYdvaj29dS2kEy$2wcdsgdsauw")
+				deKey = sessionKey.Values["Key"]
+				stringDeKey = fmt.Sprintf("%s", deKey)
+				
+				if stringDeKey == "" || len(stringDeKey) != 8 {
+					stringDeKey = "keyIdiot"
+				}
+
+				byteDeKey := []byte(stringDeKey)
+				
+				ivKey := byteDeKey
+				deMsgText, errDes := DesDecryption(byteDeKey, ivKey, Messages[i].TextMsg)
+
+				if errDes != nil {
+					fmt.Fprintf(w, "Error404")
+				}
+
+				TextMsg := string(deMsgText)
+				UserInfo := getUsers(fmt.Sprintf("SELECT id, name, password, admin, op, moder, ava, status, color FROM \"User\" WHERE id=%d", Messages[i].UserID))
+
+				Msg := DeshifrMessage{Messages[i].ID, TextMsg, Messages[i].UserID, Messages[i].ThemeID, Messages[i].PubDate, UserInfo}
+				DeshifrMessages = append(DeshifrMessages, Msg)
+			} 
+			} else {
+				for i := range Messages {
+					deMsgText := string(Messages[i].TextMsg)
+
+					TextMsg := string(deMsgText)
+					UserInfo := getUsers(fmt.Sprintf("SELECT id, name, password, admin, op, moder, ava, status, color FROM \"User\" WHERE id=%d", Messages[i].UserID))
+
+					Msg := DeshifrMessage{Messages[i].ID, TextMsg, Messages[i].UserID, Messages[i].ThemeID, Messages[i].PubDate, UserInfo}
+					DeshifrMessages = append(DeshifrMessages, Msg)
+				}
+			}
+		}
+
+		// DataTaDM Словарь для хранения Theme и DeshifrMsg, TaMD - Theme and DeshifrMsg
+
+		session, _ := store.Get(r, "User")
+		UserName := session.Values["Name"]
+		fmt.Print("UserName: ")
+		fmt.Println(UserName)
+		var data = DataTaDM{}
+		if UserName == nil {
+			data = DataTaDM{ThemeInfo: ThemeInfo, Messages: DeshifrMessages, User: nil}
+		} else {
+			type UserCookie struct {
+				Name interface{}
+				OP   interface{}
+				ID   interface{}
+			}
+
+			cookieUserName := session.Values["Name"]
+			Name := cookieUserName
+			OP := session.Values["OP"]
+
+			data = DataTaDM{
+				ThemeInfo: ThemeInfo,
+				Messages: DeshifrMessages,
+				User: UserCookie{
+					Name:  Name,
+					OP: 	 OP,
+					ID:    session.Values["ID"],
+				},
+			}
+			fmt.Println("b")
+		}
+		fmt.Println(data)
+		
+
+		tmpl, err := template.ParseFiles("templates/theme.html", "templates/header.html", "templates/footer.html")
+
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+
+		sort.Slice(data.Messages, func(i, j int) bool {return data.Messages[i].ID < data.Messages[j].ID})
+		tmpl.ExecuteTemplate(w, "theme", data)
+	}	else {
+		var url string = "/"
+		http.Redirect(w, r, url, http.StatusSeeOther)
+	}
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/register.html", "templates/header.html", "templates/footer.html")
 
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		fmt.Fprintf(w, "Error404")
 	}
 
-	tmpl.ExecuteTemplate(w, "theme", data)
+	tmpl.ExecuteTemplate(w, "register", nil)
+}
+
+func CreateAcc(w http.ResponseWriter, r *http.Request) {
+	var data = RegisterStruct{
+		Nick: r.FormValue("nick"),
+		Pass1: r.FormValue("pass1"),
+		Pass2: r.FormValue("pass2"),
+	}
+
+	if data.Pass1 == data.Pass2 {
+		userList := getUsers("SELECT name, password FROM \"User\"")
+
+		var checkUser bool = false
+
+		for i := range userList {
+			if userList[i].Name == data.Nick {
+				checkUser = true
+				break
+			}
+		}
+
+		if !checkUser {
+			db := conn()
+
+			_, DBerr := db.Exec("insert into \"User\" VALUES ((SELECT MAX(id) FROM \"User\")+1, $1, $2, false, 0, false, 'default_ava.jpg', 'Новый пользователь', '#888', false, null);", data.Nick, data.Pass2)
+
+			if DBerr != nil {
+				fmt.Fprintf(w, "Error404")
+
+				defer db.Close()
+
+				defer http.Redirect(w, r, "/register/", http.StatusSeeOther)
+			} else {
+				defer db.Close()
+
+				defer http.Redirect(w, r, "/login/", http.StatusSeeOther)
+			}
+		}
+	}
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -541,26 +718,16 @@ func Loginka(w http.ResponseWriter, r *http.Request) {
 
 			defer db.Close()
 
-			http.Redirect(w, r, "/", http.StatusSeeOther)
+			defer http.Redirect(w, r, "/", http.StatusSeeOther)
 		} else {
 			defer db.Close()
 
-			http.Redirect(w, r, "/login/", http.StatusSeeOther)
+			defer http.Redirect(w, r, "/login/", http.StatusSeeOther)
 		}
 	}
-
-	
 }
 
 func ClearCookie(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "User")
-	session.Options.MaxAge = -1
-	err := session.Save(r, w)
-
-	if err != nil {
-		log.Fatal("failed to delete session", err)
-	}
-
 	session2, _ := store.Get(r, "drYdvaj29dS2kEy$2wcdsgdsauw")
 	session2.Options.MaxAge = -1
 	err2 := session2.Save(r, w)
@@ -569,7 +736,15 @@ func ClearCookie(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("failed to delete session", err2)
 	}
 
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	session, _ := store.Get(r, "User")
+	session.Options.MaxAge = -1
+	err := session.Save(r, w)
+
+	if err != nil {
+		log.Fatal("failed to delete session", err)
+	}
+
+	defer http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func heKey(w http.ResponseWriter, r *http.Request) {
@@ -592,49 +767,60 @@ func AddMsg(w http.ResponseWriter, r *http.Request) {
 	userID := r.FormValue("userID")
 	themeID := r.FormValue("themeID")
 	msgText := r.FormValue("MsgText")
-	fmt.Println(msgText)
 
-	if len(msgText) % 8 != 0 {
-		for len(msgText) % 8 != 0 {
-			msgText = msgText + " "
+	db := conn()
+
+	themeInfo, errSQL := db.Query("SELECT anon FROM \"Theme\" WHERE id=$1", themeID)
+
+	if errSQL != nil {
+		fmt.Fprintf(w, "Error404")
+	}
+
+	type ThemeAnon struct {
+		Anon bool
+	}
+
+	themeAN := ThemeAnon{}
+
+	for themeInfo.Next() {
+		err := themeInfo.Scan(&themeAN.Anon)
+
+		if err != nil {
+			fmt.Fprintf(w, "Error404")
 		}
 	}
 
-	fmt.Println(fmt.Sprintf("\"%s\"", msgText))
+	var text []byte
+	var err error
+	if themeAN.Anon == true {
+		if len(msgText) % 8 != 0 {
+			for len(msgText) % 8 != 0 {
+				msgText = msgText + " "
+			}
+		}
 
-	session, _ := store.Get(r, "drYdvaj29dS2kEy$2wcdsgdsauw")
-	fmt.Println("b")
-	key := session.Values["Key"]
-	keyHe := fmt.Sprintf("%s", key)
-	if keyHe == "" || len(keyHe) / 8 != 1 {
-		keyHe = "keyIdiot"
+		session, _ := store.Get(r, "drYdvaj29dS2kEy$2wcdsgdsauw")
+		key := session.Values["Key"]
+		keyHe := fmt.Sprintf("%s", key)
+
+		if keyHe == "" || len(keyHe) / 8 != 1 {
+			keyHe = "keyIdiot"
+		}
+
+		keyShifr := []byte(keyHe)
+
+		textMsg := []byte(msgText)
+
+		ivKey := keyShifr
+		text, err = DesEncryption(keyShifr, ivKey, textMsg)
+		
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		text = []byte(msgText)
 	}
-	keyShifr := []byte(keyHe)
-	fmt.Println("c")
-	fmt.Println(keyHe)
 
-	textMsg := []byte(msgText)
-
-	// ivKey := []byte(string(keyShifr) + strings.Repeat("0", 16))
-	// keyShifr = []byte(string(keyShifr) + strings.Repeat("0", 16))
-	ivKey := keyShifr
-	fmt.Println(len(ivKey))
-	text, err := DesEncryption(keyShifr, ivKey, textMsg)
-	//SStextMsg := string(text)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// var listUint8 []uint8
-
-	// for i := text {
-	// 	listUIN8 = append(listUIN8, text[i])
-	// }
-	// fmt.Println(listUIN8)
-	//textMessages := string(text)
-	//fmt.Println(textMessages)
-
-	db := conn()
 	t := time.Now()
 
 	intUserID, errUserID := strconv.Atoi(userID)
@@ -647,19 +833,8 @@ func AddMsg(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(errThemeID)
 	}
 
-	fmt.Println(intThemeID)
-	fmt.Println(intUserID)
-	fmt.Println(text)
-	fmt.Println(string(text))
-
-	//codyUTF, errSqlUTF := db.Query("SET client_encoding = 'UTF8';")
-	//(SELECT MAX(id) FROM \"Message\")+
 	db.Exec("INSERT INTO \"Message\"(id, text, user_id, rubric_id, pub_date) VALUES ((SELECT MAX(id) FROm \"Message\")+1, $1, $2, $3, $4)", text, intUserID, intThemeID, t.Format(time.RFC3339))
-	// (SELECT MAX(id) FROM \"Message\")+
-	// Vars := mux.Vars(r)
-	// w.WriteHeader(http.StatusOK)
-
-	// redic := Vars["redic"]
+	
 	defer db.Close()
 
 	defer http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
@@ -668,7 +843,10 @@ func AddMsg(w http.ResponseWriter, r *http.Request) {
 func handleRequest() {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/", homePage).Methods("GET")
+	rtr.HandleFunc("/{id:[0-9]+}/", OpenType).Methods("GET")
 	rtr.HandleFunc("/theme/{id:[0-9]+}/", openTheme).Methods("GET")
+	rtr.HandleFunc("/register/", Register).Methods("GET")
+	rtr.HandleFunc("/createAcc/", CreateAcc).Methods("POST")
 	rtr.HandleFunc("/login/", Login).Methods("GET")
 	rtr.HandleFunc("/authtorization/", Loginka).Methods("POST")
 	rtr.HandleFunc("/clearCookie/", ClearCookie).Methods("GET")
@@ -678,12 +856,12 @@ func handleRequest() {
 	http.Handle("/", rtr)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	// HAHAH Danil перешел с питона на голэнг --
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "9000"
-	}
-	http.ListenAndServe(":" + port, context.ClearHandler(http.DefaultServeMux))
-	// http.ListenAndServe(":8000", nil)
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "9000"
+	// }
+	// http.ListenAndServe(":" + port, context.ClearHandler(http.DefaultServeMux))
+	http.ListenAndServe(":8000", nil)
 }
 
 func main() {
